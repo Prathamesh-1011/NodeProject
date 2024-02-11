e09efead62b76e3cd7078da5ed8589d80f417504
Prathamesh Rokade <prathameshr955@gmail.com>
2024-02-11 20:45:07 +0530

    completed Requirement 4 of Assignment 1

--- Requirement4/exit1.js
+++ Requirement4/exit1.js
@@ -1,6 +1,5 @@
 import { promises as fsPromises } from 'fs';
 
-// Attach 'exit' and 'unhandledRejection' events to the 'process' object
 process.on('exit', (code) => {
   console.log(`'exit' event called with code ${code}`);
 });
@@ -9,7 +8,6 @@ process.on('unhandledRejection', (reason, promise) => {
   console.error("Unhandled Rejection at:", promise, "reason:", reason);
 });
 
-// Promisified version of fs.readFile
 const readFileAsync = (filePath) => {
   return new Promise((resolve, reject) => {
     fsPromises.readFile(filePath, { encoding: 'utf8' })
@@ -18,16 +16,13 @@ const readFileAsync = (filePath) => {
   });
 };
 
-// Incorrect file name to trigger an exception
 const incorrectFileName = 'nonexistent-file.txt';
 
-// Call the promisified readFileAsync function
 readFileAsync(incorrectFileName)
   .then(data => {
     console.log('File content:', data);
   })
   .catch(error => {
     console.error('Error reading file:', error);
-    // Throw a new Error from catch handler
     throw new Error('Custom Error');
   });
