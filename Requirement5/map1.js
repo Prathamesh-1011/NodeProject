96443e2b242387226a21a7feb3cd95317dd7df98
Prathamesh Rokade <prathameshr955@gmail.com>
2024-02-11 20:58:01 +0530

    updated requirement 5

--- Requirement5/map1.js
+++ Requirement5/map1.js
@@ -1,16 +1,16 @@
-import response from './response.json' assert { type: "json" };
+import response from './response1.json' assert { type: "json" };
 
 const mappedArray = response.map(item => {
     return {
         name: item.name,
-        year: item.vehicle.year
+        year: item.year
     };
 });
 
 console.log("Mapped Array:");
 console.log(mappedArray);
 
-const filteredArray = response.filter(item => item.vehicle.year > 2020);
+const filteredArray = response.filter(item => item.year > 2020);
 
 console.log("\nFiltered Array:");
 console.log(filteredArray);
